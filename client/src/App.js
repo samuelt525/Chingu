f31613e5dec3269d9b2547d86052024db04a8f39
samuelt525 <samuelt525@gmail.com>
2022-07-25 00:39:00 -0400

    Created Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Messages from './pages/Messages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/"/>
+          <Route path="/Messages" component={Messages} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
---
+++ client/src/pages/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Messages(){
+    return (
+        <h1>
+            Messages
+        </h1>
+    )
+}
+export default Messages
